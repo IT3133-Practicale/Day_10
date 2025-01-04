97daad79017adbc3bad0ef5399784f3c5798c8ad
Pasindu Nuwan <154910776+Pasindunuwan2000@users.noreply.github.com>
2025-01-04 13:18:06 +0530

    feat: :sparkles: Set up navigation and initial route in App component

--- Day10/App.js
+++ Day10/App.js
@@ -2,6 +2,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Home from './components/Home';
+import ContactUs from './components/ContactUs';
+import AboutUs from './components/AboutUs';
 
 const Stack = createNativeStackNavigator();
 export default function App() {
@@ -19,7 +21,8 @@ export default function App() {
         }}
         >
           <Stack.Screen name='home' component={Home} options={{title:'Welcome'}}/>
-          
+          <Stack.Screen name='contact' component={ContactUs}options={{title:'Contact Us'}}/>
+          <Stack.Screen name='about' component={AboutUs}options={{title:'About Us'}}/>
         </Stack.Navigator>
       </NavigationContainer>
       
